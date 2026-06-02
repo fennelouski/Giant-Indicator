64a3075bbb3e6182e98d2fe5c05fca58d61b1c92
fennelouski <nathanfennel@gmail.com>
2026-06-02 11:28:43 +0200

    Initial Commit

---
+++ Giant Indicator/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Giant Indicator
+//
+//  Created by Nathan Fennel on 6/2/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Giant IndicatorTests/Giant_IndicatorTests.swift
@@ -0,0 +1,18 @@
+//
+//  Giant_IndicatorTests.swift
+//  Giant IndicatorTests
+//
+//  Created by Nathan Fennel on 6/2/26.
+//
+
+import Testing
+
+struct Giant_IndicatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
